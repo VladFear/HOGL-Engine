94a4b037c97498011e4c980f28cff491b77f6a47
Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>
2020-08-20 19:48:36 +0300

    new: Makefile: Add rules to build tests
    Add corresponding variables and rules in environment.mk
    and Makefile files.
    
    Use gtest and pthread libs for test building.
    
    Create hello world test for smoke testing.
    
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>

--- Makefile
+++ Makefile
@@ -4,12 +4,20 @@ TARGET_LIB := $(BUILD_DIR)/$(ENGINE_LIB_NAME)
 
 all: $(TARGET_LIB)
 
+tests: $(TEST_EXECS)
+
 $(TARGET_LIB): $(OBJS)
 	mkdir -p $(BUILD_DIR)
 	ar rcs $@ $^
 
 $(OBJS): $(OBJ_DIR)/%.o : $(SRC_DIR)/%.cpp | objdirs
 	$(CXX) -c $(CXXFLAGS) $< -o $@
+
+$(TEST_EXECS): $(OBJ_DIR)/%.out : %.cpp | testdirs
+	$(CXX) $< $(CXXFLAGS) -lgtest -pthread -o $@
+
+testdirs:
+	mkdir -p $(TEST_DIRS)
 
 objdirs:
 	mkdir -p $(OBJECT_DIRS)
--- environment.mk
+++ environment.mk
@@ -11,8 +11,11 @@ INC_DIR := include
 SRC_DIR := source
 OBJ_DIR := objects
 LIB_DIR := lib
+TEST_DIR := tests
 BUILD_DIR := build
+
 OBJECT_DIRS := $(subst $(SRC_DIR),$(OBJ_DIR),$(shell find $(SRC_DIR) -type d))
+TEST_DIRS := $(addprefix $(OBJ_DIR)/,$(shell find $(TEST_DIR) -type d))
 
 CXXFLAGS := -I$(INC_DIR) -g -Wall
 
@@ -20,4 +23,6 @@ ENGINE_LIB_NAME := libglobalengine.a
 
 INCS := $(shell find $(INC_DIR) -name "*.h")
 SRCS := $(shell find $(SRC_DIR) -name "*.cpp")
+TESTS := $(shell find $(TEST_DIR) -name "*.cpp")
 OBJS := $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(SRCS))
+TEST_EXECS := $(patsubst %.cpp,$(OBJ_DIR)/%.out,$(TESTS))
