dbf377cd25360ede6c9ed623e1fc7fbc3288a5ac
Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>
2020-09-17 04:22:16 +0300

    new: Overload stream operator for glm::mat4
    Function glm::to_string provides not very readable output, so
    overloading of stream operator can be used for more convenient output.
    
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>

--- include/physics/Transformations.h
+++ include/physics/Transformations.h
@@ -1,7 +1,34 @@
 #pragma once
 
+#include <boost/algorithm/string.hpp>
+#include <boost/range/algorithm_ext/erase.hpp>
+#include <boost/range/algorithm.hpp>
+#include <string>
+#include <vector>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+std::ostream& operator<<(std::ostream& os, const glm::mat4& matrix)
+{
+	std::string matrix_str = glm::to_string(matrix);
+	std::vector<std::string> vec;
+
+	// Split by vectors
+	boost::split(vec, matrix_str, boost::is_any_of("()"), boost::token_compress_on);
+	// Remove empty string
+	vec.pop_back();
+	// Remove extra strings
+	auto end = boost::range::remove_if(vec, [](const std::string& str) { return str == ", "; });
+
+	vec.erase(end, vec.end());
+
+	for (const auto & element : vec)
+		os << element << '\n';
+
+	return os;
+}
 
 glm::mat4 createTransformationMatrix(const glm::vec3 & translation,
                                      const glm::vec3 & rotation,
