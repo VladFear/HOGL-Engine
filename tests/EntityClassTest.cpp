cbacbd6b2557c01f673b56acea6954c76270527b
Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>
2020-10-15 21:13:32 +0300

    ref: Remove GE namespace in tests
    Tests is a client code and they should be included in GE namespace.
    
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>

--- tests/EntityClassTest.cpp
+++ tests/EntityClassTest.cpp
@@ -6,288 +6,284 @@
 #include "GlobalEngine.h"
 #include "Entity.h"
 
-namespace GE
+template <typename U>
+using uPtr = std::unique_ptr<U>;
+
+using vec3 = glm::vec3;
+
+class GlobalEngineTestClass : public ::testing::Test
 {
-
-	template <typename U>
-	using uPtr = std::unique_ptr<U>;
-
-	using vec3 = glm::vec3;
-
-	class GlobalEngineTestClass : public ::testing::Test
-	{
-		protected:
-			static void SetUpTestCase()
+	protected:
+		static void SetUpTestCase()
+		{
+			try
 			{
-				try
-				{
-					// TODO: Make window invisible(not working yet)
-					glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+				// TODO: Make window invisible(not working yet)
+				glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 
-					engine = std::make_unique<GlobalEngine>(EngineAPI::OpenGL);
-					ASSERT_NE(engine, nullptr);
-				}
-				catch (const std::exception & e)
-				{
-					std::cerr << e.what() << '\n';
-				}
+				engine = std::make_unique<GE::GlobalEngine>(GE::EngineAPI::OpenGL);
+				ASSERT_NE(engine, nullptr);
 			}
+			catch (const std::exception & e)
+			{
+				std::cerr << e.what() << '\n';
+			}
+		}
 
-		protected:
-			static uPtr<GlobalEngine> engine;
-	};
+	protected:
+		static uPtr<GE::GlobalEngine> engine;
+};
 
-	uPtr<GlobalEngine> GlobalEngineTestClass::engine = nullptr;
+uPtr<GE::GlobalEngine> GlobalEngineTestClass::engine = nullptr;
 
-	TEST_F(GlobalEngineTestClass, EntityDefaultConstructorTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntityDefaultConstructorTest)
+{
+	GE::Entity entity;
 
-		ASSERT_EQ(entity.getModel(), nullptr);
-		ASSERT_EQ(entity.getShaderProgram(), nullptr);
-		ASSERT_EQ(entity.getShaderProgramId(), 0);
+	ASSERT_EQ(entity.getModel(), nullptr);
+	ASSERT_EQ(entity.getShaderProgram(), nullptr);
+	ASSERT_EQ(entity.getShaderProgramId(), 0);
 
-		vec3 position = entity.getPosition();
-		EXPECT_FLOAT_EQ(position.x, 0.f);
-		EXPECT_FLOAT_EQ(position.y, 0.f);
-		EXPECT_FLOAT_EQ(position.z, 0.f);
+	vec3 position = entity.getPosition();
+	EXPECT_FLOAT_EQ(position.x, 0.f);
+	EXPECT_FLOAT_EQ(position.y, 0.f);
+	EXPECT_FLOAT_EQ(position.z, 0.f);
 
-		vec3 rotation = entity.getRotation();
-		EXPECT_FLOAT_EQ(rotation.x, 0.f);
-		EXPECT_FLOAT_EQ(rotation.y, 0.f);
-		EXPECT_FLOAT_EQ(rotation.z, 0.f);
+	vec3 rotation = entity.getRotation();
+	EXPECT_FLOAT_EQ(rotation.x, 0.f);
+	EXPECT_FLOAT_EQ(rotation.y, 0.f);
+	EXPECT_FLOAT_EQ(rotation.z, 0.f);
 
-		vec3 scaling = entity.getScalingFactor();
-		EXPECT_FLOAT_EQ(scaling.x, 1.f);
-		EXPECT_FLOAT_EQ(scaling.y, 1.f);
-		EXPECT_FLOAT_EQ(scaling.z, 1.f);
-	}
+	vec3 scaling = entity.getScalingFactor();
+	EXPECT_FLOAT_EQ(scaling.x, 1.f);
+	EXPECT_FLOAT_EQ(scaling.y, 1.f);
+	EXPECT_FLOAT_EQ(scaling.z, 1.f);
+}
 
-	TEST_F(GlobalEngineTestClass, EntityMoveConstructorTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntityMoveConstructorTest)
+{
+	GE::Entity entity;
 
-		// Set transforms params
-		auto position = vec3(3.f, 4.f, 5.f);
-		entity.setPosition(position);
+	// Set transforms params
+	auto position = vec3(3.f, 4.f, 5.f);
+	entity.setPosition(position);
 
-		auto rotation = vec3(10.f, 20.f, 50.f);
-		entity.setRotation(rotation);
+	auto rotation = vec3(10.f, 20.f, 50.f);
+	entity.setRotation(rotation);
 
-		auto scalingFactor = vec3(2.f, 4.f, 5.f);
-		entity.setScalingFactor(scalingFactor);
+	auto scalingFactor = vec3(2.f, 4.f, 5.f);
+	entity.setScalingFactor(scalingFactor);
 
-		entity.setShaderProgram(std::make_shared<ShaderProgram>());
-		ASSERT_NE(entity.getShaderProgram(), nullptr);
+	entity.setShaderProgram(std::make_shared<GE::ShaderProgram>());
+	ASSERT_NE(entity.getShaderProgram(), nullptr);
 
-		unsigned int shaderProgramId = entity.getShaderProgramId();
-		ASSERT_NE(shaderProgramId, 0);
+	unsigned int shaderProgramId = entity.getShaderProgramId();
+	ASSERT_NE(shaderProgramId, 0);
 
-		// Get model should be nullptr, as no model set
-		EXPECT_EQ(entity.getModel(), nullptr);
+	// Get model should be nullptr, as no model set
+	EXPECT_EQ(entity.getModel(), nullptr);
 
-		Entity anotherEntity(std::move(entity));
+	GE::Entity anotherEntity(std::move(entity));
 
-		ASSERT_EQ(anotherEntity.getPosition(), position);
-		ASSERT_EQ(anotherEntity.getRotation(), rotation);
-		ASSERT_EQ(anotherEntity.getScalingFactor(), scalingFactor);
+	ASSERT_EQ(anotherEntity.getPosition(), position);
+	ASSERT_EQ(anotherEntity.getRotation(), rotation);
+	ASSERT_EQ(anotherEntity.getScalingFactor(), scalingFactor);
 
-		ASSERT_NE(anotherEntity.getShaderProgram(), nullptr);
-		ASSERT_NE(anotherEntity.getShaderProgramId(), 0);
-		// Still should be nullptr
-		ASSERT_EQ(anotherEntity.getModel(), nullptr);
+	ASSERT_NE(anotherEntity.getShaderProgram(), nullptr);
+	ASSERT_NE(anotherEntity.getShaderProgramId(), 0);
+	// Still should be nullptr
+	ASSERT_EQ(anotherEntity.getModel(), nullptr);
 
-		// Shader in entity must be nullptr after move
-		ASSERT_EQ(entity.getShaderProgram(), nullptr);
-		ASSERT_EQ(entity.getShaderProgramId(), 0);
-		ASSERT_EQ(entity.getModel(), nullptr);
-	}
+	// Shader in entity must be nullptr after move
+	ASSERT_EQ(entity.getShaderProgram(), nullptr);
+	ASSERT_EQ(entity.getShaderProgramId(), 0);
+	ASSERT_EQ(entity.getModel(), nullptr);
+}
 
-	TEST_F(GlobalEngineTestClass, EntityMoveAssignmentOperatorTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntityMoveAssignmentOperatorTest)
+{
+	GE::Entity entity;
 
-		// Set transforms params
-		auto position = vec3(3.f, 4.f, 5.f);
-		entity.setPosition(position);
+	// Set transforms params
+	auto position = vec3(3.f, 4.f, 5.f);
+	entity.setPosition(position);
 
-		auto rotation = vec3(10.f, 20.f, 50.f);
-		entity.setRotation(rotation);
+	auto rotation = vec3(10.f, 20.f, 50.f);
+	entity.setRotation(rotation);
 
-		auto scalingFactor = vec3(2.f, 4.f, 5.f);
-		entity.setScalingFactor(scalingFactor);
+	auto scalingFactor = vec3(2.f, 4.f, 5.f);
+	entity.setScalingFactor(scalingFactor);
 
-		entity.setShaderProgram(std::make_shared<ShaderProgram>());
-		ASSERT_NE(entity.getShaderProgram(), nullptr);
+	entity.setShaderProgram(std::make_shared<GE::ShaderProgram>());
+	ASSERT_NE(entity.getShaderProgram(), nullptr);
 
-		unsigned int shaderProgramId = entity.getShaderProgramId();
-		ASSERT_NE(shaderProgramId, 0);
+	unsigned int shaderProgramId = entity.getShaderProgramId();
+	ASSERT_NE(shaderProgramId, 0);
 
-		// Get model should be nullptr, as no model set
-		EXPECT_EQ(entity.getModel(), nullptr);
+	// Get model should be nullptr, as no model set
+	EXPECT_EQ(entity.getModel(), nullptr);
 
-		Entity anotherEntity = std::move(entity);
+	GE::Entity anotherEntity = std::move(entity);
 
-		ASSERT_EQ(anotherEntity.getPosition(), position);
-		ASSERT_EQ(anotherEntity.getRotation(), rotation);
-		ASSERT_EQ(anotherEntity.getScalingFactor(), scalingFactor);
+	ASSERT_EQ(anotherEntity.getPosition(), position);
+	ASSERT_EQ(anotherEntity.getRotation(), rotation);
+	ASSERT_EQ(anotherEntity.getScalingFactor(), scalingFactor);
 
-		ASSERT_NE(anotherEntity.getShaderProgram(), nullptr);
-		ASSERT_NE(anotherEntity.getShaderProgramId(), 0);
-		// Still should be nullptr
-		ASSERT_EQ(anotherEntity.getModel(), nullptr);
+	ASSERT_NE(anotherEntity.getShaderProgram(), nullptr);
+	ASSERT_NE(anotherEntity.getShaderProgramId(), 0);
+	// Still should be nullptr
+	ASSERT_EQ(anotherEntity.getModel(), nullptr);
 
-		// Shader in entity must be nullptr after move
-		ASSERT_EQ(entity.getShaderProgram(), nullptr);
-		ASSERT_EQ(entity.getShaderProgramId(), 0);
-		ASSERT_EQ(entity.getModel(), nullptr);
-	}
+	// Shader in entity must be nullptr after move
+	ASSERT_EQ(entity.getShaderProgram(), nullptr);
+	ASSERT_EQ(entity.getShaderProgramId(), 0);
+	ASSERT_EQ(entity.getModel(), nullptr);
+}
 
-	TEST_F(GlobalEngineTestClass, EntitySetShaderProgramTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntitySetShaderProgramTest)
+{
+	GE::Entity entity;
 
-		ASSERT_EQ(entity.getShaderProgram(), nullptr);
+	ASSERT_EQ(entity.getShaderProgram(), nullptr);
 
-		entity.setShaderProgram(std::make_shared<ShaderProgram>());
-		ASSERT_NE(entity.getShaderProgram(), nullptr);
-	}
+	entity.setShaderProgram(std::make_shared<GE::ShaderProgram>());
+	ASSERT_NE(entity.getShaderProgram(), nullptr);
+}
 
-	TEST_F(GlobalEngineTestClass, EntitySetPositionTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntitySetPositionTest)
+{
+	GE::Entity entity;
 
-		vec3 position = entity.getPosition();
-		EXPECT_FLOAT_EQ(position.x, 0.f);
-		EXPECT_FLOAT_EQ(position.y, 0.f);
-		EXPECT_FLOAT_EQ(position.z, 0.f);
+	vec3 position = entity.getPosition();
+	EXPECT_FLOAT_EQ(position.x, 0.f);
+	EXPECT_FLOAT_EQ(position.y, 0.f);
+	EXPECT_FLOAT_EQ(position.z, 0.f);
 
-		entity.setPosition(vec3(4.f, 5.f, 6.f));
-		position = entity.getPosition();
-		ASSERT_FLOAT_EQ(position.x, 4.f);
-		ASSERT_FLOAT_EQ(position.y, 5.f);
-		ASSERT_FLOAT_EQ(position.z, 6.f);
+	entity.setPosition(vec3(4.f, 5.f, 6.f));
+	position = entity.getPosition();
+	ASSERT_FLOAT_EQ(position.x, 4.f);
+	ASSERT_FLOAT_EQ(position.y, 5.f);
+	ASSERT_FLOAT_EQ(position.z, 6.f);
 
-		// Reassigning values
-		entity.setPosition(vec3(6.f, 7.f, 8.f));
-		position = entity.getPosition();
-		ASSERT_FLOAT_EQ(position.x, 6.f);
-		ASSERT_FLOAT_EQ(position.y, 7.f);
-		ASSERT_FLOAT_EQ(position.z, 8.f);
-	}
+	// Reassigning values
+	entity.setPosition(vec3(6.f, 7.f, 8.f));
+	position = entity.getPosition();
+	ASSERT_FLOAT_EQ(position.x, 6.f);
+	ASSERT_FLOAT_EQ(position.y, 7.f);
+	ASSERT_FLOAT_EQ(position.z, 8.f);
+}
 
-	TEST_F(GlobalEngineTestClass, EntitySetRotationTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntitySetRotationTest)
+{
+	GE::Entity entity;
 
-		vec3 rotation = entity.getRotation();
-		EXPECT_FLOAT_EQ(rotation.x, 0.f);
-		EXPECT_FLOAT_EQ(rotation.y, 0.f);
-		EXPECT_FLOAT_EQ(rotation.z, 0.f);
+	vec3 rotation = entity.getRotation();
+	EXPECT_FLOAT_EQ(rotation.x, 0.f);
+	EXPECT_FLOAT_EQ(rotation.y, 0.f);
+	EXPECT_FLOAT_EQ(rotation.z, 0.f);
 
-		entity.setRotation(vec3(4.f, 5.f, 6.f));
-		rotation = entity.getRotation();
-		ASSERT_FLOAT_EQ(rotation.x, 4.f);
-		ASSERT_FLOAT_EQ(rotation.y, 5.f);
-		ASSERT_FLOAT_EQ(rotation.z, 6.f);
+	entity.setRotation(vec3(4.f, 5.f, 6.f));
+	rotation = entity.getRotation();
+	ASSERT_FLOAT_EQ(rotation.x, 4.f);
+	ASSERT_FLOAT_EQ(rotation.y, 5.f);
+	ASSERT_FLOAT_EQ(rotation.z, 6.f);
 
-		// Reassigning values
-		entity.setRotation(vec3(6.f, 7.f, 8.f));
-		rotation = entity.getRotation();
-		ASSERT_FLOAT_EQ(rotation.x, 6.f);
-		ASSERT_FLOAT_EQ(rotation.y, 7.f);
-		ASSERT_FLOAT_EQ(rotation.z, 8.f);
-	}
+	// Reassigning values
+	entity.setRotation(vec3(6.f, 7.f, 8.f));
+	rotation = entity.getRotation();
+	ASSERT_FLOAT_EQ(rotation.x, 6.f);
+	ASSERT_FLOAT_EQ(rotation.y, 7.f);
+	ASSERT_FLOAT_EQ(rotation.z, 8.f);
+}
 
-	TEST_F(GlobalEngineTestClass, EntitySetScalingFactorTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntitySetScalingFactorTest)
+{
+	GE::Entity entity;
 
-		vec3 scaling = entity.getScalingFactor();
-		EXPECT_FLOAT_EQ(scaling.x, 1.f);
-		EXPECT_FLOAT_EQ(scaling.y, 1.f);
-		EXPECT_FLOAT_EQ(scaling.z, 1.f);
+	vec3 scaling = entity.getScalingFactor();
+	EXPECT_FLOAT_EQ(scaling.x, 1.f);
+	EXPECT_FLOAT_EQ(scaling.y, 1.f);
+	EXPECT_FLOAT_EQ(scaling.z, 1.f);
 
-		entity.setScalingFactor(vec3(4.f, 5.f, 6.f));
-		scaling = entity.getScalingFactor();
-		ASSERT_FLOAT_EQ(scaling.x, 4.f);
-		ASSERT_FLOAT_EQ(scaling.y, 5.f);
-		ASSERT_FLOAT_EQ(scaling.z, 6.f);
+	entity.setScalingFactor(vec3(4.f, 5.f, 6.f));
+	scaling = entity.getScalingFactor();
+	ASSERT_FLOAT_EQ(scaling.x, 4.f);
+	ASSERT_FLOAT_EQ(scaling.y, 5.f);
+	ASSERT_FLOAT_EQ(scaling.z, 6.f);
 
-		// Reassigning values
-		entity.setScalingFactor(vec3(6.f, 7.f, 8.f));
-		scaling = entity.getScalingFactor();
-		ASSERT_FLOAT_EQ(scaling.x, 6.f);
-		ASSERT_FLOAT_EQ(scaling.y, 7.f);
-		ASSERT_FLOAT_EQ(scaling.z, 8.f);
-	}
+	// Reassigning values
+	entity.setScalingFactor(vec3(6.f, 7.f, 8.f));
+	scaling = entity.getScalingFactor();
+	ASSERT_FLOAT_EQ(scaling.x, 6.f);
+	ASSERT_FLOAT_EQ(scaling.y, 7.f);
+	ASSERT_FLOAT_EQ(scaling.z, 8.f);
+}
 
-	TEST_F(GlobalEngineTestClass, EntityRotateTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntityRotateTest)
+{
+	GE::Entity entity;
 
-		vec3 rotation = entity.getRotation();
-		EXPECT_FLOAT_EQ(rotation.x, 0.f);
-		EXPECT_FLOAT_EQ(rotation.y, 0.f);
-		EXPECT_FLOAT_EQ(rotation.z, 0.f);
+	vec3 rotation = entity.getRotation();
+	EXPECT_FLOAT_EQ(rotation.x, 0.f);
+	EXPECT_FLOAT_EQ(rotation.y, 0.f);
+	EXPECT_FLOAT_EQ(rotation.z, 0.f);
 
-		entity.rotate(vec3(4.f, 5.f, 6.f));
-		rotation = entity.getRotation();
-		ASSERT_FLOAT_EQ(rotation.x, 4.f);
-		ASSERT_FLOAT_EQ(rotation.y, 5.f);
-		ASSERT_FLOAT_EQ(rotation.z, 6.f);
+	entity.rotate(vec3(4.f, 5.f, 6.f));
+	rotation = entity.getRotation();
+	ASSERT_FLOAT_EQ(rotation.x, 4.f);
+	ASSERT_FLOAT_EQ(rotation.y, 5.f);
+	ASSERT_FLOAT_EQ(rotation.z, 6.f);
 
-		entity.rotate(vec3(6.f, 7.f, 8.f));
-		rotation = entity.getRotation();
-		ASSERT_FLOAT_EQ(rotation.x, 10.f);
-		ASSERT_FLOAT_EQ(rotation.y, 12.f);
-		ASSERT_FLOAT_EQ(rotation.z, 14.f);
-	}
+	entity.rotate(vec3(6.f, 7.f, 8.f));
+	rotation = entity.getRotation();
+	ASSERT_FLOAT_EQ(rotation.x, 10.f);
+	ASSERT_FLOAT_EQ(rotation.y, 12.f);
+	ASSERT_FLOAT_EQ(rotation.z, 14.f);
+}
 
-	TEST_F(GlobalEngineTestClass, EntityScaleTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntityScaleTest)
+{
+	GE::Entity entity;
 
-		vec3 scaling = entity.getScalingFactor();
-		EXPECT_FLOAT_EQ(scaling.x, 1.f);
-		EXPECT_FLOAT_EQ(scaling.y, 1.f);
-		EXPECT_FLOAT_EQ(scaling.z, 1.f);
+	vec3 scaling = entity.getScalingFactor();
+	EXPECT_FLOAT_EQ(scaling.x, 1.f);
+	EXPECT_FLOAT_EQ(scaling.y, 1.f);
+	EXPECT_FLOAT_EQ(scaling.z, 1.f);
 
-		entity.scale(vec3(4.f, 5.f, 6.f));
-		scaling = entity.getScalingFactor();
-		ASSERT_FLOAT_EQ(scaling.x, 4.f);
-		ASSERT_FLOAT_EQ(scaling.y, 5.f);
-		ASSERT_FLOAT_EQ(scaling.z, 6.f);
+	entity.scale(vec3(4.f, 5.f, 6.f));
+	scaling = entity.getScalingFactor();
+	ASSERT_FLOAT_EQ(scaling.x, 4.f);
+	ASSERT_FLOAT_EQ(scaling.y, 5.f);
+	ASSERT_FLOAT_EQ(scaling.z, 6.f);
 
-		entity.scale(vec3(6.f, 7.f, 8.f));
-		scaling = entity.getScalingFactor();
-		ASSERT_FLOAT_EQ(scaling.x, 24.f);
-		ASSERT_FLOAT_EQ(scaling.y, 35.f);
-		ASSERT_FLOAT_EQ(scaling.z, 48.f);
-	}
+	entity.scale(vec3(6.f, 7.f, 8.f));
+	scaling = entity.getScalingFactor();
+	ASSERT_FLOAT_EQ(scaling.x, 24.f);
+	ASSERT_FLOAT_EQ(scaling.y, 35.f);
+	ASSERT_FLOAT_EQ(scaling.z, 48.f);
+}
 
-	TEST_F(GlobalEngineTestClass, EntityMoveTest)
-	{
-		Entity entity;
+TEST_F(GlobalEngineTestClass, EntityMoveTest)
+{
+	GE::Entity entity;
 
-		vec3 position = entity.getPosition();
-		EXPECT_FLOAT_EQ(position.x, 0.f);
-		EXPECT_FLOAT_EQ(position.y, 0.f);
-		EXPECT_FLOAT_EQ(position.z, 0.f);
+	vec3 position = entity.getPosition();
+	EXPECT_FLOAT_EQ(position.x, 0.f);
+	EXPECT_FLOAT_EQ(position.y, 0.f);
+	EXPECT_FLOAT_EQ(position.z, 0.f);
 
-		entity.move(vec3(4.f, 5.f, 6.f));
-		position = entity.getPosition();
-		ASSERT_FLOAT_EQ(position.x, 4.f);
-		ASSERT_FLOAT_EQ(position.y, 5.f);
-		ASSERT_FLOAT_EQ(position.z, 6.f);
+	entity.move(vec3(4.f, 5.f, 6.f));
+	position = entity.getPosition();
+	ASSERT_FLOAT_EQ(position.x, 4.f);
+	ASSERT_FLOAT_EQ(position.y, 5.f);
+	ASSERT_FLOAT_EQ(position.z, 6.f);
 
-		entity.move(vec3(6.f, 7.f, 8.f));
-		position = entity.getPosition();
-		ASSERT_FLOAT_EQ(position.x, 10.f);
-		ASSERT_FLOAT_EQ(position.y, 12.f);
-		ASSERT_FLOAT_EQ(position.z, 14.f);
-	}
-} // GE
+	entity.move(vec3(6.f, 7.f, 8.f));
+	position = entity.getPosition();
+	ASSERT_FLOAT_EQ(position.x, 10.f);
+	ASSERT_FLOAT_EQ(position.y, 12.f);
+	ASSERT_FLOAT_EQ(position.z, 14.f);
+}
 
 int main(int argc, char ** argv)
 {
