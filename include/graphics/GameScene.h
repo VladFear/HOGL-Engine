ebbcd0920424f01a6b3bae87d213ba7ee731414a
Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>
2020-11-12 22:54:41 +0200

    fix: Remove dead code from GameScene class
    Signed-off-by: Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>

--- include/graphics/GameScene.h
+++ include/graphics/GameScene.h
@@ -16,13 +16,9 @@ namespace GE
 
 			// Actions
 			void addGameObject(Shared<IDrawable> object);
-			void renderScene() const;
 
 			// Getters
 			[[nodiscard]] std::forward_list<Shared<IDrawable>> & getGameObjectsList();
-
-		private:
-			void renderObject(Shared<IDrawable> object) const;
 
 		private:
 			std::forward_list<Shared<IDrawable>> m_objectsList;
--- src/graphics/GameScene.cpp
+++ src/graphics/GameScene.cpp
@@ -8,17 +8,6 @@ namespace GE
 		m_objectsList.push_front(std::move(object));
 	}
 
-	void GameScene::renderScene() const
-	{
-		for (const auto & object : m_objectsList)
-			renderObject(object);
-	}
-
-	void GameScene::renderObject(Shared<IDrawable> object) const
-	{
-		object->draw();
-	}
-
 	std::forward_list<Shared<IDrawable>> & GameScene::getGameObjectsList()
 	{
 		return m_objectsList;
